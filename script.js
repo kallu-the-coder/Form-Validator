b32904cc1ea446f2471c21477704738de8583da2
kallu-the-coder <lalit.coder@gmail.com>
2022-10-19 12:58:54 +0530

    form

---
+++ script.js
@@ -0,0 +1,90 @@
+const form = document.getElementById('form');
+
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const phone = document.getElementById('phonenumber');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input,message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+    
+}
+
+
+// function password 
+function checkPassword(input1 , input2){
+
+    if(input1.value === "123456789" || input1.value.toLowerCase() === "password"){
+        showError(input1,'This cannot be the password')
+    }
+    else{
+         checkPasswordMatch(input1,input2);
+    }
+    
+}
+
+// function password match
+function checkPasswordMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2,'Password doesn\'t Matched ')
+    }
+}
+// check email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(String(email.value).toLowerCase())){
+       showSuccess(email);
+    }else{
+        showError(email,'Email is not valid');
+    }
+    
+}
+
+// check required field
+function checkRequired(inputArr){
+  inputArr.forEach(input => {
+       if(input.value.trim() === ''){
+           showError(input,`${getFieldName(input)} is required`)
+       }else{
+           showSuccess(input);
+       }
+  });
+}
+
+// Get field Name
+ function getFieldName(input){
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+ }
+
+ // check input length
+ function checkLength(input,min,max){
+     if(input.value.length<min){
+         showError(input,`${getFieldName(input)} must be atleast ${min} characters`)
+     }else if(input.value.length>max){
+        showError(input,`${getFieldName(input)} must not exceed ${max} characters`)
+     }else{
+         showSuccess(input);
+     }
+ }
+
+// Event Listeners
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+   
+    checkRequired([username,email,password,password2]);
+     checkLength(username,5, 20);
+     checkLength(password,8, 20);
+     checkLength(phone,10, 10);
+     isValidEmail(email);
+     checkPassword(password,password2);
+});
+
